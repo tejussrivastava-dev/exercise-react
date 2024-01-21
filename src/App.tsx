a0dda31aea85839ab4a38695076fd406cb123277
tejussrivastava-dev <142511137+tejussrivastava-dev@users.noreply.github.com>
2024-01-22 00:22:13 +0530

    boilplate code

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+import "./App.css";
+import MainContent from "./Component/MainContent/MainContent";
+
+function App() {
+  return <MainContent />;
+}
+
+export default App;
